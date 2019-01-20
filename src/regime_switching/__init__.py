ef3563ea8a2c229834ffdc6e206d9c0f4ae61e3e
Anatoly Makarevich <anatoly_mak@yahoo.com>
2019-01-20 23:28:52 +0300

    Added package.

---
+++ src/regime_switching/__init__.py
@@ -0,0 +1,5 @@
+"""Regime-Switching Time Series Models
+
+"""
+
+__version__ = '0.1.0'
---
+++ src/setup.py
@@ -0,0 +1,22 @@
+
+from setuptools import setup, find_packages
+
+from regime_switching import __version__ 
+
+setup(
+    name="regime_switching",
+    description="Regime-switching time series models",
+    version=__version__,
+    author="Anatoly Makarevich",
+    author_email="anatoly_mak@yahoo.com",  
+    packages=find_packages(),
+    install_requires=[
+        'numpy', 'pandas', 
+        'scikit-learn', 
+        'statsmodels', 
+    ],
+    # TODO: Add extras and such
+    # Testing (Nose tests) 
+    # Docs (Sphinx) 
+    # Optional dependencies? 
+)
