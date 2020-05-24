a868be1213136b7db65d883c067fafd11fa88c40
Anatoly Makarevich <anatoly_mak@yahoo.com>
2020-05-24 21:15:47 +0300

    rng now uses new numpy api

--- regime_switching/utils/rng.py
+++ regime_switching/utils/rng.py
@@ -1,13 +1,16 @@
-from typing import Union
-from numpy.random import RandomState
 from copy import deepcopy
+from typing import Union
+
+from numpy.random import Generator, default_rng
+
+AnyRandomState = Union[int, Generator, None]
 
 
-def fix_rng(random_state: Union[int, RandomState, None] = None) -> RandomState:
+def fix_rng(random_state: AnyRandomState = None) -> Generator:
     """Helper function for copying a random state."""
 
-    if isinstance(random_state, RandomState):
+    if isinstance(random_state, Generator):
         random_state = deepcopy(random_state)
     else:
-        random_state = RandomState(random_state)
+        random_state = default_rng(random_state)
     return random_state
